44bcd16bb9917e01706eff2c7224c8ffe6567491
Putributarbutar <35138911+Putributarbutar@users.noreply.github.com>
2018-01-08 17:49:15 +0700

    Add files via upload

---
+++ Logika.java
@@ -0,0 +1,23 @@
+public class Logika 
+
+{
+
+	public static void main(String args[]) 
+
+{
+
+		boolean b1=true,b2=false;
+
+		int a=3,b=7;
+
+		System.out.println(b1 && b2);
+
+		System.out.println(b1 || b2);
+
+		System.out.println(b1 ^ b2);
+
+		System.out.println(a > b);
+
+		System.out.println(a <= 7);
+}
+}
