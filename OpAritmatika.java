bad70b1c3e4d4a7c452cee6d65b333313776f435
Putributarbutar <35138911+Putributarbutar@users.noreply.github.com>
2018-01-10 18:33:03 +0700

    Add files via upload

---
+++ OpAritmatika.java
@@ -0,0 +1,19 @@
+public class OpAritmatika {
+
+	public static void main(String args[]) {
+
+	System.out.println(5%3);
+
+	int a=0;
+
+
+		System.out.println(a++ * 3);
+
+		System.out.println(a);	
+
+		System.out.println(++a * 2);
+
+		System.out.println(a);
+}
+
+}
